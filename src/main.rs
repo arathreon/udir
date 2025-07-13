ec158c93bcf7e5747c7bc27a1bb876e68e8ba74b
arathreon <arathreon@users.noreply.github.com>
2025-07-13 19:55:58 +0200

    Took out parts of main function to main_inner for better testability. Started working on a test.

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn get_files_and_directories(
 fn create_directories(list_of_directories: &Vec<DirectoryToCreate>) -> Vec<DirectoryToCreate> {
     let len_directories = list_of_directories.len();
 
-    if (len_directories == 0) {
+    if len_directories == 0 {
         return Vec::new();
     }
 
@@ -117,7 +117,7 @@ fn create_directories(list_of_directories: &Vec<DirectoryToCreate>) -> Vec<Direc
 fn copy_files(list_of_files: &Vec<FileToCopy>) -> Vec<FileToCopy> {
     let len_files = list_of_files.len();
 
-    if (len_files == 0) {
+    if len_files == 0 {
         return Vec::new();
     }
 
@@ -320,7 +320,7 @@ mod tests {
     fn test_create_directories() {
         // Set up files
         let current_path = env::current_dir().unwrap();
-        let test_dir_path = current_path.join("test_dir");
+        let test_dir_path = current_path.join("test_dir_create_directories");
         let existing_dir_path = test_dir_path.join("existing_dir");
 
         // Delete all test directories and files
@@ -394,7 +394,7 @@ mod tests {
     fn test_copy_files() {
         // Set up paths
         let current_path = env::current_dir().unwrap();
-        let test_dir_path = current_path.join("test_dir");
+        let test_dir_path = current_path.join("test_dir_copy_files");
         let source_dir_path = test_dir_path.join("source_dir");
         let source_subdir_1_path = source_dir_path.join("subdir_1");
         let source_subdir_2_path = source_dir_path.join("subdir_2");
@@ -481,6 +481,12 @@ mod tests {
         // Check that files expected to fail failed
         assert_eq!(result, expected_failed_files);
 
+        // Check that nothing happened to the source files content
+        assert_eq!(fs::read(&source_file_1).unwrap(), source_file_1_content);
+        assert_eq!(fs::read(&source_file_2).unwrap(), source_file_2_content);
+        assert_eq!(fs::read(&source_file_3).unwrap(), source_file_3_content);
+        assert_eq!(fs::read(&source_file_4).unwrap(), source_file_4_content);
+
         // Check that copied files have the correct content
         assert_eq!(fs::read(&target_file_1).unwrap(), source_file_1_content);
         assert_eq!(fs::read(&target_file_2).unwrap(), source_file_2_content);
@@ -492,6 +498,124 @@ mod tests {
 
         // Delete all test directories and files
         fs::remove_dir_all(test_dir_path).unwrap();
+    }
+
+    #[test]
+    fn test_main_inner() {
+        // Set up files
+        let current_path = env::current_dir().unwrap();
+        let test_dir_path = current_path.join("test_dir_main_inner");
+        let source_dir_path = test_dir_path.join("source_dir");
+        let source_subdir_1_path = source_dir_path.join("subdir_subdir_1");
+        let source_subdir_2_path = source_dir_path.join("subdir_subdir_2");
+        let target_dir_path = test_dir_path.join("target_dir");
+        let target_subdir_1_path = target_dir_path.join("subdir_subdir_1");
+        let target_subdir_2_path = target_dir_path.join("subdir_subdir_2");
+        let target_subdir_3_path = target_dir_path.join("subdir_subdir_3");
+
+        // Delete all test directories and files
+        match fs::remove_dir_all(&test_dir_path) {
+            Ok(_) => {}
+            Err(_) => println!("[INFO] Test dir couldn't be removed"),
+        };
+
+        // Create test directories
+        fs::create_dir(&test_dir_path).unwrap();
+        fs::create_dir(&source_dir_path).unwrap();
+        fs::create_dir(&source_subdir_1_path).unwrap();
+        fs::create_dir(&source_subdir_2_path).unwrap();
+        fs::create_dir(&target_dir_path).unwrap();
+        fs::create_dir(&target_subdir_1_path).unwrap();
+        fs::create_dir(&target_subdir_3_path).unwrap();
+
+        // Write files where target should be overwritten
+        let target_file_1 = target_dir_path.join("test_1.txt");
+        let source_file_1 = source_dir_path.join("test_1.txt");
+        let source_file_1_content = b"This is new text in file 1";
+        fs::write(&target_file_1, b"This is old text in file 1").unwrap();
+        sleep(Duration::from_nanos(1)); // waiting so the source file is newer
+        fs::write(&source_file_1, &source_file_1_content).unwrap();
+
+        // Write files that should stay the same
+        let target_file_2 = target_dir_path.join("test_2.txt");
+        let source_file_2 = source_dir_path.join("test_2.txt");
+        let source_file_2_content = b"This is unchanged text in file 2";
+        fs::write(&target_file_2, source_file_2_content).unwrap();
+        fs::copy(&target_file_2, &source_file_2).unwrap();
+        assert_eq!(
+            fs::metadata(&source_file_2).unwrap().modified().unwrap(),
+            fs::metadata(&target_file_2).unwrap().modified().unwrap(),
+        );
+
+        // Write files that should stay the same in subdirectory 1
+        let target_file_3 = target_subdir_1_path.join("test_3.txt");
+        let source_file_3 = source_subdir_1_path.join("test_3.txt");
+        let source_file_3_content = b"This is unchanged text in file 3";
+        fs::write(&target_file_3, &source_file_3_content).unwrap();
+        fs::copy(&target_file_3, &source_file_3).unwrap();
+        assert_eq!(
+            fs::metadata(&source_file_3).unwrap().modified().unwrap(),
+            fs::metadata(&target_file_3).unwrap().modified().unwrap(),
+        );
+
+        // Write files that should be changed in subdirectory 1
+        let target_file_4 = target_subdir_1_path.join("test_4.txt");
+        let source_file_4 = source_subdir_1_path.join("test_4.txt");
+        let source_file_4_content = b"This is new text in file 4";
+        fs::write(&target_file_4, b"This is old text in file 4").unwrap();
+        sleep(Duration::from_nanos(1)); // waiting so the source file is newer
+        fs::write(&source_file_4, &source_file_4_content).unwrap();
+
+        // Write a file that should be created in subdirectory 1
+        let target_file_5 = target_subdir_1_path.join("test_5.txt");
+        let source_file_5 = source_subdir_1_path.join("test_5.txt");
+        let source_file_5_content = b"This is new text in file 5";
+        fs::write(&source_file_5, &source_file_5_content).unwrap();
+
+        // Write a file that should be created in subdirectory 2
+        let target_file_6 = target_subdir_2_path.join("test_6.txt");
+        let source_file_6 = source_subdir_2_path.join("test_6.txt");
+        let source_file_6_content = b"This is new text in file 6";
+        fs::write(&source_file_6, &source_file_6_content).unwrap();
+
+        // Write a file that should stay in target subdirectory 3
+        let target_file_7 = target_subdir_3_path.join("test_7.txt");
+        let target_file_7_content = b"This is a relict that should not be touched file 7";
+        fs::write(&target_file_7, &target_file_7_content).unwrap();
+
+        // Run the tested function
+        main_inner(&source_dir_path, &target_dir_path);
+
+        // Check that files expected to exist actually exist and have the correct content.
+        assert_eq!(fs::read(&target_file_1).unwrap(), source_file_1_content);
+        assert_eq!(fs::read(&target_file_2).unwrap(), source_file_2_content);
+        assert_eq!(fs::read(&target_file_3).unwrap(), source_file_3_content);
+    }
+}
+
+fn main_inner(source: &PathBuf, target: &PathBuf) {
+    let directories;
+    let files;
+    let results = get_files_and_directories(source, target)
+        .expect("Files and directories could not be generated!");
+    files = results.files;
+    directories = results.directories;
+
+    let failed_directories = create_directories(&directories);
+    let failed_files = copy_files(&files);
+
+    if failed_directories.len() > 0 {
+        println!("Failed to create directories:");
+        for directory in failed_directories {
+            println!("    {}", directory.path.display());
+        }
+    }
+
+    if failed_files.len() > 0 {
+        println!("Failed to copy files:");
+        for file in failed_files {
+            println!("    {}", file.source.display());
+        }
     }
 }
 
@@ -520,27 +644,5 @@ fn main() {
     println!("Source dir: {}", &source.display());
     println!("Target dir: {}", &target.display());
 
-    let directories;
-    let files;
-    let results = get_files_and_directories(&source, &target)
-        .expect("Files and directories could not be generated!");
-    files = results.files;
-    directories = results.directories;
-
-    let failed_directories = create_directories(&directories);
-    let failed_files = copy_files(&files);
-
-    if failed_directories.len() > 0 {
-        println!("Failed to create directories:");
-        for directory in failed_directories {
-            println!("    {}", directory.path.display());
-        }
-    }
-
-    if failed_files.len() > 0 {
-        println!("Failed to copy files:");
-        for file in failed_files {
-            println!("    {}", file.source.display());
-        }
-    }
+    main_inner(&source, &target);
 }
