3bb201b75472717ef2c50f7ec06125c68ba194a7
arathreon <arathreon@users.noreply.github.com>
2026-02-24 00:15:28 +0100

    Directories are copied only when they are not in the directories_to_skip

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -34,7 +34,8 @@ pub(crate) fn get_files_and_directories(
         for entry in fs::read_dir(source)? {
             let entry = entry?;
             let source_path = entry.path();
-            if source_path.is_dir() {
+            let is_entry_dir = source_path.is_dir();
+            if source_path.is_dir() && !directories_to_skip.contains(&source_path){
                 // If the source_path is a subdirectory, check whether it exists. If not, add it
                 // to be created. Call the function on the subdirectory.
                 let dir_name = source_path.file_name().unwrap();
@@ -48,7 +49,7 @@ pub(crate) fn get_files_and_directories(
                 let mut result = get_files_and_directories(&source_path, &new_target, directories_to_skip)?;
                 files.append(&mut result.files);
                 directories.append(&mut result.directories);
-            } else {
+            } else if !is_entry_dir {
                 // Source path is a file
                 let file_name = source_path.file_name().unwrap();
                 let target_path = Path::new(target).join(Path::new(file_name));
@@ -238,6 +239,132 @@ mod tests {
         fs::write(&target_file_7, &target_file_7_content).unwrap();
 
         let mut results = get_files_and_directories(&source_dir_path, &target_dir_path, &HashSet::new()).unwrap();
+
+        results.files.sort_by_key(|val| val.source.clone());
+
+        assert_eq!(
+            results,
+            FilesAndDirectories {
+                files: vec![
+                    FileToCopy {
+                        source: source_file_4,
+                        target: target_file_4,
+                    },
+                    FileToCopy {
+                        source: source_file_5,
+                        target: target_file_5,
+                    },
+                    FileToCopy {
+                        source: source_file_6,
+                        target: target_file_6,
+                    },
+                    FileToCopy {
+                        source: source_file_1,
+                        target: target_file_1,
+                    },
+                ],
+                directories: vec![DirectoryToCreate {
+                    path: target_subdir_2_path,
+                }]
+            }
+        );
+
+        // Delete all test directories and files
+        fs::remove_dir_all(test_dir_path).unwrap();
+    }
+
+    #[test]
+    fn test_get_files_and_directories_with_skipped_directories() {
+        // Set up files
+        let current_path = env::current_dir().unwrap();
+        let test_dir_path = current_path.join("test_dir");
+        let source_dir_path = test_dir_path.join("source_dir");
+        let source_subdir_1_path = source_dir_path.join("subdir_1");
+        let source_subdir_2_path = source_dir_path.join("subdir_2");
+        let source_subdir_4_path = source_dir_path.join("subdir_4");  // This shall be ignored
+        let target_dir_path = test_dir_path.join("target_dir");
+        let target_subdir_1_path = target_dir_path.join("subdir_1");
+        let target_subdir_2_path = target_dir_path.join("subdir_2");
+        let target_subdir_3_path = target_dir_path.join("subdir_3");
+
+        // Delete all test directories and files
+        match fs::remove_dir_all(&test_dir_path) {
+            Ok(_) => {}
+            Err(_) => println!("[INFO] Test dir couldn't be removed"),
+        };
+
+        // Create test directories
+        fs::create_dir(&test_dir_path).unwrap();
+        fs::create_dir(&source_dir_path).unwrap();
+        fs::create_dir(&source_subdir_1_path).unwrap();
+        fs::create_dir(&source_subdir_2_path).unwrap();
+        fs::create_dir(&source_subdir_4_path).unwrap();
+        fs::create_dir(&target_dir_path).unwrap();
+        fs::create_dir(&target_subdir_1_path).unwrap();
+        fs::create_dir(&target_subdir_3_path).unwrap();
+
+        // Write files where target should be overwritten
+        let target_file_1 = target_dir_path.join("test_1.txt");
+        let source_file_1 = source_dir_path.join("test_1.txt");
+        let source_file_1_content = b"This is some newer text";
+        fs::write(&target_file_1, b"This is some text").unwrap();
+        sleep(Duration::from_nanos(1)); // waiting so the source file is newer
+        fs::write(&source_file_1, &source_file_1_content).unwrap();
+
+        // Write files that should stay the same
+        let target_file_2 = target_dir_path.join("test_2.txt");
+        let source_file_2 = source_dir_path.join("test_2.txt");
+        let source_file_2_content = b"This is unchanged text";
+        fs::write(&target_file_2, source_file_2_content).unwrap();
+        fs::copy(&target_file_2, &source_file_2).unwrap();
+        assert_eq!(
+            fs::metadata(&source_file_2).unwrap().modified().unwrap(),
+            fs::metadata(&target_file_2).unwrap().modified().unwrap(),
+        );
+
+        // Write files that should stay the same in subdirectory 1
+        let target_file_3 = target_subdir_1_path.join("test_3.txt");
+        let source_file_3 = source_subdir_1_path.join("test_3.txt");
+        let source_file_3_content = b"This is unchanged text too";
+        fs::write(&target_file_3, &source_file_3_content).unwrap();
+        fs::copy(&target_file_3, &source_file_3).unwrap();
+        assert_eq!(
+            fs::metadata(&source_file_3).unwrap().modified().unwrap(),
+            fs::metadata(&target_file_3).unwrap().modified().unwrap(),
+        );
+
+        // Write files that should be changed in subdirectory 1
+        let target_file_4 = target_subdir_1_path.join("test_4.txt");
+        let source_file_4 = source_subdir_1_path.join("test_4.txt");
+        let source_file_4_content = b"4 This is some changed text in subdirectory 1";
+        fs::write(&target_file_4, b"4 This is some text in subdirectory 1").unwrap();
+        sleep(Duration::from_nanos(1)); // waiting so the source file is newer
+        fs::write(&source_file_4, &source_file_4_content).unwrap();
+
+        // Write a file that should be created in subdirectory 1
+        let target_file_5 = target_subdir_1_path.join("test_5.txt");
+        let source_file_5 = source_subdir_1_path.join("test_5.txt");
+        let source_file_5_content = b"5 This is some new text in subdirectory 1";
+        fs::write(&source_file_5, &source_file_5_content).unwrap();
+
+        // Write a file that should be created in subdirectory 2
+        let target_file_6 = target_subdir_2_path.join("test_6.txt");
+        let source_file_6 = source_subdir_2_path.join("test_6.txt");
+        let source_file_6_content = b"6 This is some new text in subdirectory 1";
+        fs::write(&source_file_6, &source_file_6_content).unwrap();
+
+        // Write a file that should stay in target subdirectory 3
+        let target_file_7 = target_subdir_3_path.join("test_7.txt");
+        let target_file_7_content = b"7 This is a relict that should not be touched";
+        fs::write(&target_file_7, &target_file_7_content).unwrap();
+
+        let source_file_8 = source_subdir_4_path.join("test_8.txt");
+        let source_file_8_content = b"8 This file should be ignored";
+        fs::write(&source_file_8, &source_file_8_content).unwrap();
+
+        let directories_to_skip = HashSet::from([source_subdir_4_path.clone()]);
+
+        let mut results = get_files_and_directories(&source_dir_path, &target_dir_path, &directories_to_skip).unwrap();
 
         results.files.sort_by_key(|val| val.source.clone());
 
